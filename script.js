4e38402453aa91ed92beca5ca29178448445663f
kaur-Gurdeep <gagakaur188@gmail.com>
2024-11-09 15:44:49 -0800

    responsive-grid commit

--- script.js
+++ script.js
@@ -35,35 +35,86 @@ function run(diceNum){
     });
 }
 
-function move(direction){
-    return new Promise(async(resolve, reject) => {
-    // new Audio('dice-sound.mp3').play(); ////player move sound
-        if (direction === 'up') {
-            document.querySelector(`#${turn}`).style.marginTop = String(marginTop() - 9.8) + 'vmin';
-        } else if (direction === 'right') {
-            document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() + 9.8) + 'vmin';
-        } else if (direction === 'left') {
-            document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() - 9.8) + 'vmin';
+function getDirection() {
+    let currentLeft = marginLeft();
+    let currentTop = marginTop();
+
+    // 1. Move up if at the far-right end of an even row
+    if (currentLeft >= 88 && Math.abs(currentTop % (-19.6)) < 0.1) {
+        return 'up';
+    }
+    // 2. Move up if at the far-left end of an odd row
+    else if (currentLeft <= 0 && Math.abs(currentTop % (-19.6)) > 0.1) {
+        return 'up';
+    }
+    // 3. If on an even row (multiple of -19.6 top), move right
+    else if (Math.abs(currentTop % (-19.6)) < 0.1) {
+        return 'right';
+    }
+    // 4. If on an odd row, move left
+    else {
+        return 'left';
+    }
+}
+
+function move(direction) {
+    return new Promise(async (resolve) => {
+        console.log('Direction:', direction);
+        console.log('Current marginTop:', marginTop());
+        
+        if (direction === 'up' && marginTop() > 0) {
+            console.log('Moving Up');
+            document.querySelector(`#${turn}`).style.marginTop = String(marginTop() - 9) + 'vmin';
+        } 
+        else if (direction === 'right' && marginLeft() < 88) {
+            console.log('Moving Right');
+            document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() + 9) + 'vmin';
+        } 
+        else if (direction === 'left' && marginLeft() > 0) {
+            console.log('Moving Left');
+            document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() - 9) + 'vmin';
         }
         await new Promise(resolve => setTimeout(resolve, 400));
         resolve();
     });
 }
 
-function getDirection(){
-    let direction;
-    if((marginLeft() == 88.2 && ((((marginTop()*10)%(-19.6*10))/10) == 0)) || (marginLeft()==0 && ((((marginTop()*10)%(-19.6*10))/10)!=0))){
-        direction = 'up'
-    }
-    else if((((marginTop()*10)%(-19.6*10))/10)==0){
-        direction = "right";
-    }
-    else{
-        direction = "left";
-    }
-    return direction;
 
-}
+
+
+
+// function move(direction){
+//     return new Promise(async(resolve, reject) => {
+//     // new Audio('dice-sound.mp3').play(); ////player move sound
+//         if (direction === 'up') {
+//             document.querySelector(`#${turn}`).style.marginTop = String(marginTop() - 9) + 'vmin';
+//         } else if (direction === 'right') {
+//             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() + 9) + 'vmin';
+//         } else if (direction === 'left') {
+//             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() - 9) + 'vmin';
+//         }
+//         await new Promise(resolve => setTimeout(resolve, 400));
+//         resolve();
+//     });
+// }
+
+// function getDirection(){
+//     let direction;
+//     if((marginLeft() == 88.2 && ((((marginTop()*10)%(-19.6*10))/10) == 0)) || (marginLeft()==0 && ((((marginTop()*10)%(-19.6*10))/10)!=0))){
+//         direction = 'up'
+//     }
+//     else if((((marginTop()*10)%(-19.6*10))/10)==0){
+//         direction = "right";
+//     }
+//     else{
+//         direction = "left";
+//     }
+//     return direction;
+
+// }
+
+
+
 function marginLeft(){
     return Number(document.querySelector(`#${turn}`).style.marginLeft.split('v')[0])
 }
@@ -116,44 +167,21 @@ function marginTop(){
 
 
 
+// // Sample JavaScript to switch turns
+// let currentPlayer = "Red";
 
-// boxNumbers();
-
-// document.addEventListener("keydown", (e) => {   
-//     if (e.keyCode == 83) { // 'S' key has keycode 83
-//         let diceNum = roll();
-//     }
-// });
-
-// function roll() {
-//     let diceNum = Math.floor(Math.random() * 6) + 1;
-//     let values = [[0, -360], [-180, -360], [-180, -270], [0, -90], [270, 180], [90, 90]];
-
-//     const audio = new Audio(`dice-sound.mp3`);
-//     audio.play().catch(error => {
-//         console.error("Audio playback failed:", error);
-//     });
-    
-//     // Apply initial transform to reset
-//     document.querySelector('#cube_inner').style.transform = `rotateX(360deg) rotateY(360deg)`;
-    
-//     // Set timeout to apply the new rotation after a brief delay for smoother animation
-//    document.querySelector('#cube_inner').style.transform = `rotateX(${values[diceNum-1][0]}deg) rotateY(${values[diceNum-1][1]}deg)`
-//     // setTimeout(() => {
-//     //     document.querySelector('#cube_inner').style.transform = `rotateX(${values[diceNum - 1][0]}deg) rotateY(${values[diceNum - 1][1]}deg)`;
-//     // }, 50);
-
-//     return diceNum;
+// function updateTurnIndicator() {
+//     const turnIndicator = document.getElementById("turnIndicator");
+//     turnIndicator.textContent = `${currentPlayer} Player's Turn`;
+//     turnIndicator.style.color = currentPlayer === "Red" ? "#ff4d4d" : "#4d94ff"; // Update color for each player
 // }
 
-// function boxNumbers() {
-//     let boxes = document.querySelectorAll('.box');
-
-//     boxes.forEach((box, i) => {
-//         if (String(i).length === 1 || (String(i).length === 2 && Number(String(i)[0]) % 2 === 0)) {
-//             box.innerHTML = 100 - i;
-//         } else {
-//             box.innerHTML = Number(`${9 - Number(String(i)[0])}${String(i)[1]}`) + 1;
-//         }
-//     });
+// function toggleTurn() {
+//     currentPlayer = currentPlayer === "Red" ? "Blue" : "Red";
+//     updateTurnIndicator();
 // }
+
+// // Call updateTurnIndicator initially to set the first player's turn
+// updateTurnIndicator();
+
+// Example: Call toggleTurn() to switch turns after each player moves
