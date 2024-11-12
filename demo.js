297bb44cfb9740835cdc44230abfb6061c3398e3
kaur-Gurdeep <gagakaur188@gmail.com>
2024-11-12 13:48:12 -0800

    adjust players position

--- demo.js
+++ demo.js
@@ -1,322 +1,214 @@
-// let turn = 'red';
-// let stopEvent = false;
-// // let redPosition = 0;
-// // let bluePosition = 0;
-// // let totalBoxes = 100; // Number of boxes on the board
-
-// document.querySelector('#red').style.marginLeft = '0vmin;';
-// document.querySelector('#red').style.marginTop = '0vmin;';
-// document.querySelector('#blue').style.marginLeft = '0vmin;';
-// document.querySelector('#blue').style.marginTop = '0vmin;';
-// boxNumbers();
-
-// document.addEventListener("keydown", async(e) => {   
-//     if (e.keyCode == 83 && !stopEvent){  // Check for 'S' key
-//         stopEvent = true;
-//         let diceNum = await roll();
-//         await new Promise(resolve => setTimeout(resolve, 400)); // before run
-//         await run(diceNum);
-//         await new Promise(resolve => setTimeout(resolve, 400)) //after run
-//         stopEvent = false;
-//     }
-// });
-
-// // function chnangeTurn(){
-
-// // }
-
-// function run(diceNum){
-//     return new Promise(async(resolve,reject) => {
-//         for(i=1; i<=diceNum; i++){
-//             let direction = getDirection();
-//             await move(direction);
-//         }
-//         resolve();
-//     });
-// }
-
-// function getDirection() {
-//     let currentLeft = marginLeft();
-//     let currentTop = marginTop();
-
-//     // 1. Move up if at the far-right end of an even row
-//     if (currentLeft >= 88 && Math.abs(currentTop % (-19.6)) < 0.1) {
-//         return 'up';
-//     }
-//     // 2. Move up if at the far-left end of an odd row
-//     else if (currentLeft <= 0 && Math.abs(currentTop % (-19.6)) > 0.1) {
-//         return 'up';
-//     }
-//     // 3. If on an even row (multiple of -19.6 top), move right
-//     else if (Math.abs(currentTop % (-19.6)) < 0.1) {
-//         return 'right';
-//     }
-//     // 4. If on an odd row, move left
-//     else {
-//         return 'left';
-//     }
-// }
-
-// function move(direction) {
-//     return new Promise(async (resolve) => {
-//         console.log('Direction:', direction);
-//         console.log('Current marginTop:', marginTop());
-        
-//         if (direction === 'up' && marginTop() > 0) {
-//             console.log('Moving Up');
-//             document.querySelector(`#${turn}`).style.marginTop = String(marginTop() - 9) + 'vmin';
-//         } 
-//         else if (direction === 'right' && marginLeft() < 88) {
-//             console.log('Moving Right');
-//             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() + 9) + 'vmin';
-//         } 
-//         else if (direction === 'left' && marginLeft() > 0) {
-//             console.log('Moving Left');
-//             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() - 9) + 'vmin';
-//         }
-//         await new Promise(resolve => setTimeout(resolve, 400));
-//         resolve();
-//     });
-// }
 
 
-
-
-
-// // function move(direction){
-// //     return new Promise(async(resolve, reject) => {
-// //     // new Audio('dice-sound.mp3').play(); ////player move sound
-// //         if (direction === 'up') {
-// //             document.querySelector(`#${turn}`).style.marginTop = String(marginTop() - 9) + 'vmin';
-// //         } else if (direction === 'right') {
-// //             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() + 9) + 'vmin';
-// //         } else if (direction === 'left') {
-// //             document.querySelector(`#${turn}`).style.marginLeft = String(marginLeft() - 9) + 'vmin';
-// //         }
-// //         await new Promise(resolve => setTimeout(resolve, 400));
-// //         resolve();
-// //     });
-// // }
-
-// // function getDirection(){
-// //     let direction;
-// //     if((marginLeft() == 88.2 && ((((marginTop()*10)%(-19.6*10))/10) == 0)) || (marginLeft()==0 && ((((marginTop()*10)%(-19.6*10))/10)!=0))){
-// //         direction = 'up'
-// //     }
-// //     else if((((marginTop()*10)%(-19.6*10))/10)==0){
-// //         direction = "right";
-// //     }
-// //     else{
-// //         direction = "left";
-// //     }
-// //     return direction;
-
-// // }
-
-
-
-// function marginLeft(){
-//     return Number(document.querySelector(`#${turn}`).style.marginLeft.split('v')[0])
-// }
-
-// function marginTop(){
-//     return Number(document.querySelector(`#${turn}`).style.marginTop.split('v')[0])
-
-// }
-//     function roll(){
-//         return new Promise(async(resolve, reject) => {
-//             let diceNum = Math.floor(Math.random() * 6) + 1;
-//             // let values = [[0, -360], [-100, -340], [-200, -320], [-300, -300], [-400, -280], [-500, -260]];
-//             let values = [[0, -360], [-180, -360], [-180, -270], [0, -90], [270, 180], [90, 90]];
-//             new Audio(`dice-sound.mp3`).play();
-//             document.querySelector('#cube_inner').style.transform = `rotateX(360deg) rotateY(360deg)`
-//             await new Promise(resolve => setTimeout(resolve, 750))
-//             document.querySelector('#cube_inner').style.transform = `rotateX(${values[diceNum-1][0]}deg) rotateY(${values[diceNum-1][1]}deg)`
-//             await new Promise(resolve => setTimeout(resolve, 750));
-//             resolve(diceNum);
-//         });
-//     }
-//     function boxNumbers() {
-//         // let boxes = document.querySelectorAll('.box');
-
-//         // boxes.forEach((box, i) => {
-//         //     if (String(i).length === 1 || (String(i).length === 2 && Number(String(i)[0]) % 2 === 0)) {
-//         //         box.innerHTML = 100 - i;
-//         //     } else {
-//         //         //  box.innerHTML = Number(`${9 - Number(String(i)[0])}${String(i)[1]}`) + 1;
-//         //         let firstDigit = Math.floor(i / 10);
-//         //         let secondDigit = i % 10;
-//         //         box.innerHTML = 9 - firstDigit + secondDigit + 1;
-//         //     }
-//         // });
-//         let boxes = document.querySelectorAll('.box');
-
-//     boxes.forEach((box, i) => {
-//         // Calculate row and column based on a 10x10 grid
-//         let row = Math.floor(i / 10);
-//         let col = i % 10;
-
-//         // Determine the direction (left-to-right or right-to-left) based on the row index
-//         if (row % 2 === 0) {
-//             box.innerHTML = 100 - (row * 10 + col);
-//         } else {
-//             box.innerHTML = 100 - (row * 10 + (9 - col));
-//         }
-//     });
-//     }
-
-
-
-// // // Sample JavaScript to switch turns
-// // let currentPlayer = "Red";
-
-// // function updateTurnIndicator() {
-// //     const turnIndicator = document.getElementById("turnIndicator");
-// //     turnIndicator.textContent = `${currentPlayer} Player's Turn`;
-// //     turnIndicator.style.color = currentPlayer === "Red" ? "#ff4d4d" : "#4d94ff"; // Update color for each player
-// // }
-
-// // function toggleTurn() {
-// //     currentPlayer = currentPlayer === "Red" ? "Blue" : "Red";
-// //     updateTurnIndicator();
-// // }
-
-// // // Call updateTurnIndicator initially to set the first player's turn
-// // updateTurnIndicator();
-
-// // Example: Call toggleTurn() to switch turns after each player moves
-
-
-
-
-/* body {
-  background-color: lightgrey;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-#main {
-  position: absolute;
-  margin: auto;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  border: 0.1vmin solid black;
-  height: 98vmin;
-  width: 98vmin;
-}
-
-.box {
-  float: left;
-  height: 9.601vmin;
-  width: 9.601vmin;
-  border: 0.1vmin solid black;
-  text-align: center;
-  line-height: 9.601vmin;
-  font-size: 2.5vmin;
-  background-color: #a9d18e;
-}
-
-#side {
-  position: absolute;
-  top: 1vmin;
-  left: 1vmin;
-  height: 25vmin;
-  width: 35vmin;
-  border: 0.2vmin solid black;
-  background-color: white;
-  text-align: center;
-  font-size: 3.5vmin;
-}
-
-#cube_outer {
-  height: 8vmin;
-  width: 8vmin;
-  perspective: 30vmin;
-  margin: auto;
-}
-
-#cube_inner {
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  transform-style: preserve-3d;
-  transition: all 0.75s linear;
-}
-
-#cube_inner div {
-  height: 100%;
-  width: 100%;
-  position: absolute;
-
-
-
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-}
-#front { 
-    transform: translateZ(4vmin); 
-}
-#back { 
-    transform: rotateY(180deg) translateZ(4vmin); 
-}
-#left {
-    transform: rotateY(-90deg) translateX(-4vmin);
-    transform-origin: left; 
-}
-#right { 
-    transform: rotateY(90deg) translateX(4vmin); 
-    transform-origin: right;
-}
-#top { 
-    transform: rotateX(-90deg) translateY(-4vmin); 
-    transform-origin: top;
-}
-#bottom { 
-    transform: rotateX(90deg) translateY(4vmin); 
-    transform-origin: bottom;
-}
-img {
-  height: 100.3%;
-  width: 100.3%;
-}
-.players {
-  position: absolute;
-  top: 90vmin;
-  left: 3vmin;
-  height: 4vmin;
-  width: 4vmin;
-  border-radius: 50%;
-  opacity: 0.7;
-  transition-duration: 0.4s;
-  transition-property: margin;
-}
-
-.players:nth-child(1) {
-  background-color: red;
-}
-.players:nth-child(2) {
-  background-color: blue;
-}
-#main img{
+body {
+    background-color: lightgrey;
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 0;
+    height: 100vh;
+  }
+  
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    background-color: bisque;
+  }
+  
+  #gameBoard {
+    background-color: #a59a9a;
+    color: white;
+    text-align: center;
+    padding: 5px;
+  }
+  
+  #turnIndicator {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #ff4d4d; /* Red color for the red player's turn */
+  }
+  /* #main {
+    background-color: black;
+    border: 2px solid black;
+    max-width: 90vmin;
+    width: 90vw;
+    height: 90vw; /* Use 90vw for consistent square aspect ratio */
+    /*max-height: 90vmin;
+    margin: 5px auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+  } */
+  
+  #gameContainer {  
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr); /* 10 columns */
+    grid-template-rows: repeat(10, 1fr); /* 10 rows */
+    max-width: 90vmin;
+    width: 90vw;
+    height: 90vw; 
+    max-height: 90vmin;
+    pointer-events: none; 
+    overflow: hidden;
+    margin: 5px auto;
+  }
+  
+  .box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: calc(1vmin + 0.5rem);
+    background-color: #4ca5dd;
+    color: white;
+    border: 0.1vmin solid black;
+    box-sizing: border-box;
+    transition: background-color 0.3s ease; /* Optional: Add hover effect */
+  }
+  
+  .players {
     position: absolute;
-}
-#l1{
-    transform: scale(0.3) scaleX(0.6) scaleY(0.8) rotate(15deg);
-    top: 34.3vmin;
-    left: -29.5vmin;
-}
-#l2{
-    transform: scale(0.3) scaleX(0.6) scaleY(0.8) rotate(15deg);
-    top: 5vmin;
-    left: 11vmin;
-}
-#s1{
-    transform: scale(0.4) scaleY(0.8) rotate(341deg);
-    top: -27vmin;
-    left: -41vmin;
-}
- */
+    width: 5%;
+    height: 5%;
+    border-radius: 50%;
+    opacity: 0.8;
+    transition: transform 0.4s ease, margin 0.4s ease; 
+    /* transition: transform 0.4s ease;  */
+    /* bottom: 1vmin; 
+    left: 1vmin;  */
+    /* bottom: 2.8%;
+    left: 2.9%; */
+    transform: translate(50%, -50%);
+  } 
+  
+  .players#red {
+    background-color: red;
+    bottom: 0;
+    left: 0;
+    
+  }
+  
+  .players#blue {
+    background-color: blue;
+    bottom: 0;
+    left: 0;
+    
+  }
+  
+  /* Dice and Player Turn Indicator */
+  #side {
+    position: relative;
+    display: flex;
+    height: 150px;
+    width: 50%;
+    flex-direction: column;
+    justify-content: center;
+    background-color: white;
+    padding: 1rem;
+    border-radius: 1vmin;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+    text-align: center;
+    font-size: calc(1vmin + 1rem);
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-bottom: 10px;
+    margin-top: 20px;
+  }
+  
+  #p_turn {
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+  
+  #cube_outer {
+    height: 8vmin;
+    width: 8vmin;
+    perspective: 30vmin;
+    margin: auto;
+  }
+  
+  #cube_inner {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    transform-style: preserve-3d;
+    transition: all 0.75s linear;
+  }
+  
+  #cube_inner div {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
+  
+  #front {
+    transform: translateZ(4vmin);
+  }
+  
+  #back {
+    transform: rotateY(180deg) translateZ(4vmin);
+  }
+  
+  #left {
+    transform: rotateY(-90deg) translateX(-4vmin);
+    transform-origin: left;
+  }
+  
+  #right {
+    transform: rotateY(90deg) translateX(4vmin);
+    transform-origin: right;
+  }
+  
+  #top {
+    transform: rotateX(-90deg) translateY(-4vmin);
+    transform-origin: top;
+  }
+  
+  #bottom {
+    transform: rotateX(90deg) translateY(4vmin);
+    transform-origin: bottom;
+  }
+  
+  img {
+    height: 100.3%;
+    width: 100.3%;
+  }
+  
+  /* Responsive Design */
+  @media (min-width: 37.5em) /*600px / 16px = 37.5em*/ {
+    body {
+      height: 100vh;
+    }
+    .box {
+      font-size: calc(0.8vmin + 0.5rem);
+    }
+    #side {
+      background-color: gray;
+      width: 30%;
+      bottom: 1%;
+      left: 50%;
+      font-size: calc(1vmin + 0.5rem);
+    }
+    #players{
+      position: absolute;
+      transform: translate(50%, -50%);
+    }
+    .players#red {
+      background-color: rgb(68, 152, 59);
+      bottom: 0;
+      left: 0;    
+    }
+    
+    .players#blue {
+      background-color: rgb(159, 40, 157);
+      bottom: 0;
+      left: 0;
+      
+    }
+  }
+  
--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// Variables
 let turn = 'red';
 let stopEvent = false;
-let redPosition = 0;
-let bluePosition = 0;
-let totalBoxes = 100; // Number of boxes on the board
+let redPosition = 1;  // Starting at 1
+let bluePosition = 1;
 let currentPlayer = 'Red';
 
-// Initialize positions
-document.querySelector('#red').style.marginLeft = '0vmin';
-document.querySelector('#red').style.marginTop = '0vmin';
-document.querySelector('#blue').style.marginLeft = '0vmin';
-document.querySelector('#blue').style.marginTop = '0vmin';
+// Constants
+const boxSize = 9; // Each box is 9vmin
+const maxColumns = 10; // 10 columns per row
+const maxRows = 10; // 10 rows in total
+
+// Initialize positions inside the grid (row 0, col 0)
+document.querySelector('#red').style.marginLeft = '0vmin';  // Left margin of 0
+document.querySelector('#red').style.marginBottom = `0vmin`;  // Bottom margin of 0
+
+document.querySelector('#blue').style.marginLeft = '0vmin';  // Left margin of 0
+document.querySelector('#blue').style.marginBottom = `0vmin`;  // Bottom margin of 0
+
 boxNumbers();
 updateTurnIndicator();
 
@@ -30,109 +35,42 @@ document.addEventListener("keydown", async (e) => {
 // Main function to move player
 async function run(diceNum) {
     for (let i = 1; i <= diceNum; i++) {
-        let direction = getDirection();
-        await move(direction);
-
-        // Check if we need to move up after 10 moves
-        if (i % 10 === 0) {
-            await move('up'); // Move up after 10 horizontal moves
+        // Move the current player
+        if (turn === 'red') {
+            redPosition++;
+            await movePlayer(redPosition);
+        } else {
+            bluePosition++;
+            await movePlayer(bluePosition);
         }
     }
 }
-function getDirection() {
-    let currentLeft = marginLeft();
-    let currentTop = marginTop();
 
-    const boxSize = 9; // Each box is 9vmin
-    const maxColumns = 10;  // 10 columns (0 to 9) per row
+async function movePlayer(position) {
+    let player = document.querySelector(`#${turn}`);
+    
+    // Calculate row and column based on position
+    let row = Math.floor((position - 1) / maxColumns);
+    let col = (row % 2 === 0) ? (position - 1) % maxColumns : (maxColumns - 1) - ((position - 1) % maxColumns);
 
-    let row = Math.floor(currentTop / boxSize);  // 0-indexed row
-    let col = Math.floor(currentLeft / boxSize); // 0-indexed column
+    // Set new position using `left` and `bottom` properties
+    let newBottom = row * boxSize;
+    let newLeft = col * boxSize;
 
-    console.log("Row:", row, "Column:", col);
+    player.style.bottom = newBottom + 'vmin';
+    player.style.left = newLeft + 'vmin';
 
-    // Check if we're at the last column
-    if (col === maxColumns - 1) {
-        console.log("Moving Up");
-        return 'up';
-    }
-
-    // Check if we're at the first column and moving up
-    if (col === 0 && row > 0) {
-        console.log("Moving Up");
-        return 'up';
-    }
-
-    // Determine direction based on row
-    if (row % 2 === 0) { // Even row
-        console.log("Moving Right");
-        return 'right';
-    } else { // Odd row
-        console.log("Moving Left");
-        return 'left';
-    }
-}
-// // Get the direction the player needs to move
-// function getDirection() {
-//     let currentLeft = marginLeft();
-//     let currentTop = marginTop();
-
-//     const boxSize = 9; // Each box is 9vmin
-//     const maxColumns = 10;  // 10 columns (0 to 9) per row
-
-//     let row = Math.floor(currentTop / boxSize);  // 0-indexed row
-//     let col = Math.floor(currentLeft / boxSize); // 0-indexed column
-
-//     console.log("Row:", row, "Column:", col);
-
-//     // Check if we're at the last column
-//     if (col === maxColumns - 1) {
-//         console.log("Moving Up");
-//         return 'up';
-//     }
-
-//     if (row % 2 === 0) { // Even row
-//         console.log("Moving Right");
-//         return 'right';
-//     } else { // Odd row
-//         console.log("Moving Left");
-//         return 'left';
-//     }
-// }
-
-async function move(direction) {
-    const boxSize = 9; // Each box is 9vmin
-
-    if (direction === 'up' && marginTop() > 0) {
-        document.querySelector(`#${turn}`).style.marginTop = (marginTop() - boxSize) + 'vmin';
-    } else if (direction === 'right' && marginLeft() < (boxSize * 9)) { // Prevent overflow past the grid's right side
-        document.querySelector(`#${turn}`).style.marginLeft = (marginLeft() + boxSize) + 'vmin';
-    } else if (direction === 'left' && marginLeft() > 0) {
-        document.querySelector(`#${turn}`).style.marginLeft = (marginLeft() - boxSize) + 'vmin';
-    } else if (direction === 'up' && marginLeft() === (boxSize * 9)) { // Move up after reaching the last column
-        document.querySelector(`#${turn}`).style.marginTop = (marginTop() - boxSize) + 'vmin';
-        document.querySelector(`#${turn}`).style.marginLeft = (marginLeft()) + 'vmin'; // Stay in the same column
-    }
-    await new Promise(resolve => setTimeout(resolve, 400)); // Wait after each move
+    await new Promise(resolve => setTimeout(resolve, 400));
 }
 
-// Get current margin left value (current horizontal position of the player)
-function marginLeft() {
-    return Number(document.querySelector(`#${turn}`).style.marginLeft.split('v')[0]);
-}
 
-// Get current margin top value (current vertical position of the player)
-function marginTop() {
-    return Number(document.querySelector(`#${turn}`).style.marginTop.split('v')[0]);
-}
-
-// Roll the dice and animate the dice roll
+// Roll the dice and animate
 function roll() {
     return new Promise(async (resolve) => {
         let diceNum = Math.floor(Math.random() * 6) + 1;
-        let values = [[0, -360], [-180, -360], [-180, -270], [0, -90], [270, 180], [90, 90]]; // Dice rotations
-        new Audio(`dice-sound.mp3`).play();
-        document.querySelector('#cube_inner').style.transform = `rotateX(360deg) rotateY(360deg)`;
+        let values = [[0, -360], [-180, -360], [-180, -270], [0, -90], [270, 180], [90, 90]];
+        new Audio('dice-sound.mp3').play();
+        document.querySelector('#cube_inner').style.transform = 'rotateX(360deg) rotateY(360deg)';
         await new Promise(resolve => setTimeout(resolve, 750));
         document.querySelector('#cube_inner').style.transform = `rotateX(${values[diceNum - 1][0]}deg) rotateY(${values[diceNum - 1][1]}deg)`;
         await new Promise(resolve => setTimeout(resolve, 750));
@@ -140,17 +78,18 @@ function roll() {
     });
 }
 
-// Label each box with its number
+// Label each box
 function boxNumbers() {
     let boxes = document.querySelectorAll('.box');
     boxes.forEach((box, i) => {
-        let row = Math.floor(i / 10);
-        let col = i % 10;
+        let row = Math.floor(i / maxColumns);
+        let col = i % maxColumns;
 
+        // Add numbers to boxes based on row and column positioning
         if (row % 2 === 0) {
-            box.innerHTML = 100 - (row * 10 + col);
+            box.innerHTML = 100 - (row * maxColumns + col);
         } else {
-            box.innerHTML = 100 - (row * 10 + (9 - col));
+            box.innerHTML = 100 - (row * maxColumns + (maxColumns - 1 - col));
         }
     });
 }
